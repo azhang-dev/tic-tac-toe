55bcd6eff114dc9a15db2a958041b18e18d81efa
Angela Zhang <a_zhang93@hotmail.com>
2022-01-12 10:58:19 +1100

    second commit

--- js/main.js
+++ js/main.js
@@ -3,33 +3,29 @@ $(document).ready(function(){
     console.log(`DOM loaded!`);
    
     let playerTurn = 0;
-    const arrayPlayer1 = []; // indexcell clicked will be pushed and saved into this array
-    const arrayPlayer2 = []; // indexcell clicked will be pushed and saved into this array
+    const arrayPlayer1 = []; // cellId clicked will saved into this array
+    const arrayPlayer2 = []; // cellId clicked will saved into this array
    
     $('.cell').one('click',function(){
 
-        const cellBox = $(this);
-        const cellId = parseInt(cellBox.attr("id"));// get the id num of the cellBox
-       
+        const cellId = parseInt($(this).attr("id"));// get the id num of the 
+        
        if(playerTurn === 0 ){
-            cellBox.html("O");
-            cellBox.css('color','rgb(5, 128, 128)');
-            console.log(`Player 1 clicked cell`,cellId);
+            
+            $(this).html("O");
+            $(this).css('color','rgb(5, 128, 128)');
             arrayPlayer1.push(cellId);// adds and stores the index cell into the player's array
-            console.log(arrayPlayer1);// prints the array with the cells clicked
-            playerTurn = 1 // alternate to Player 2
-            checkWinningConditions(arrayPlayer1);
+            checkWinningConditions(arrayPlayer1);// first checks winning index
+            playerTurn = 1 // then alternate to Player 2
+        
         }else{
-            cellBox.html("X");
-            cellBox.css('color','rgb(5, 128, 128)');
-            console.log(`Player 2 clicked cell`,cellId);
+            $(this).html("X");
+            $(this).css('color','rgb(5, 128, 128)');
             arrayPlayer2.push(cellId);
-            console.log(arrayPlayer2);
-            playerTurn = 0 // switch back to Player 1
             checkWinningConditions(arrayPlayer2);
+            playerTurn = 0 // switch back to Player 1
         }
         
-
     });//
 
     const winningConditions = [
@@ -43,58 +39,59 @@ $(document).ready(function(){
         [0,4,8],
     ];
 
-    const checkWinningConditions = function(playerMoves){
-        console.log(`Inside checkWinningCOnditions`);
-        for(let i = 0; i < winningConditions.length; i++){
+    const checkWinningConditions = function(playerMoves){//for each player turn, this function will run.
+        
+        for(let i = 0; i < winningConditions.length; i++){// will loop each winning combinations array
+            
             let winCondition = winningConditions[i];
-            //console.log(`winCondition array:`,winCondition);
+            //console.log(`winCondition array:`,winCondition);//--> test check
             let includeCounter = 0;
-            for(let j = 0 ; j < winCondition.length; j++){ // looping each index for each array
+            let losingCounter = 0;
+
+            for(let j = 0 ; j < winCondition.length; j++){ // looping each index within each winning array
                 const winIndex = winCondition[j];
-                //console.log(`WinningIndex:`,winIndex);
-                if(playerMoves.includes(winIndex)){
-                    includeCounter ++ // ++ saving addition into the includeCounter
+                if(playerMoves.includes(winIndex)){//check against player's array to see if winning index is included
+                    includeCounter ++ // ++ saving addition into the includeCounter   
                 }
             }
-            console.log(`includeCounter:`,includeCounter);
-            if(includeCounter === 3){
+            if(includeCounter === 3){//counter if the players have 3 winning indexes included to return true
                 console.log(`You've won the game!`);
+            
+                if(playerTurn === 0){
+                    const score = parseInt($(`#player1`).text());
+                    const newScore = $(`#player1`).text(score+1);
+                } else if(playerTurn === 1){
+                    const score = parseInt($(`#player2`).text());
+                    const newScore = $(`#player2`).text(score+1);
+                } // logs the score for the winning player
                 return true;
+            } //If() counter for 3 winning index
+            else if (includeCounter !== 3){
+                console.log(`Game Over`)
             }
             
         } // outerForLoop WinningConditions
-        console.log(`sorry, you haven't won yet`);
-        return false;
-        
-      
-    };// checkWinningConditions()
+        let remainingTurns = 9;
+        // if(){
+            console.log(`sorry, you haven't won yet`); // outside of the first loop
+            return false;
+        // }
     
 
-    // const player2 = $('.cell').one('click',function(e){
-    //     if(turnPlayer === false){
-    //         const cellIndexNum = $(this);
-    //         cellIndexNum.css('border','5px solid green')
-    //         // cellIndexNum.append('<img src= images/cross.png>');
-    //         console.log(`cell clicked`, cellIndexNum);
-    //     }
+    };// checkWinningConditions()
+    // const gameOver = function(){
 
-    // });//
+    //     if(winningConditions)
+    // }
+
+    // const endGame = function (){
+
+
+    // }
+
+
+
 
 
 });//$document.ready()
 
-//const turnPlayer = function(numOfTurns){
-    //     // const player1 =[];
-    //     // const player2 =[];
-    //     if(numOfTurns % 2 == 0){
-    //         console.log(`even number: Turn for Player 1`);
-    //         return numOfTurns
-    //     } else {
-    //         console.log(`odd number: Turn for Player 2`);
-    //         return numOfTurns
-    //     } 
-    // }
-
-    // turnPlayer(1);
-    // turnPlayer(4);
-    // turnPlayer(5);
